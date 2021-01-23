64d885537a696567ea4a70ba00857e9430f7d2ba
rgalicia0729 <rigo.galicia@icloud.com>
2021-01-23 09:59:22 -0600

    Variables y Strings

---
+++ language/01_variables.py
@@ -0,0 +1,9 @@
+# Variables
+
+price = 9.99
+
+discount = 0.2
+
+total = price * (1 - discount)
+
+print(total)
---
+++ language/code.py
@@ -0,0 +1,11 @@
+
+a = 25
+b = a
+
+print(a)
+print(b)
+
+b = 17
+
+print(a)
+print(b)
