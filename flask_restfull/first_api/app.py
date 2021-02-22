f73b4b2df8c95e825fae5e9e110e62186bafc494
rgalicia0729 <rigo.galicia@icloud.com>
2021-02-22 17:36:10 -0600

    Implementing JWT Authorization with flask-restfull

--- flask_restfull/first_api/app.py
+++ flask_restfull/first_api/app.py
@@ -1,10 +1,16 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse, request
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 products = []
 
 app = Flask(__name__)
+app.secret_key = 'RUBYhn10vjFXO94T5GbsqSkelzoDHZfQ'
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
 
 parser = reqparse.RequestParser()
 parser.add_argument('id', type=int)
@@ -12,6 +18,7 @@ parser.add_argument('name', type=str)
 
 
 class Product(Resource):
+    @jwt_required()
     def get(self, productId):
         # Recurso para obtener los datos de un producto
         product = next(
@@ -19,6 +26,7 @@ class Product(Resource):
 
         return product, 200 if product is not None else 400
 
+    @jwt_required()
     def put(self, productId):
         # Recurso para actualizar los dato de un producto
         args = parser.parse_args()
@@ -35,12 +43,22 @@ class Product(Resource):
 
         return {'ok': False, 'message': 'No se encontró el producto'}, 404
 
+    @jwt_required()
+    def delete(self, productId):
+        # Recurso para eliminar un registro de un producto
+        global products
+
+        products = list(filter(lambda x: x['id'] != productId, products))
+        return {'ok': True, 'message': 'Product deleted'}
+
 
 class Products(Resource):
+    @jwt_required()
     def get(self):
         # Recurso util para consultar todos los productos
         return products
 
+    @jwt_required()
     def post(self):
         # Recurso para agregar un nuevo producto
         body = request.get_json()
